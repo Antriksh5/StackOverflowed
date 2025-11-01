5ae89e39163015105fe47f1085634cf6e8bc4601
madhur-j-10 <madhurj262@gmail.com>
2025-11-01 09:34:49 +0530

    first commit, basic controller

---
+++ pn5/src/main/java/com/stackOverFlowed/pn5/Pn5Application.java
@@ -0,0 +1,13 @@
+package com.stackOverFlowed.pn5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pn5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pn5Application.class, args);
+	}
+
+}
---
+++ pn5/src/test/java/com/stackOverFlowed/pn5/Pn5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackOverFlowed.pn5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pn5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
